6f4a998fd86f53fd510c2c23800f0c53620e10e7
tjdwhd70599@gmail.com <tjdwhd70599@gmail.com>
2021-12-26 17:58:35 +0900

    인스타그램 만들기

---
+++ lib/feed_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:instagrams/widget/post.dart';
+
+class FeedScreen extends StatelessWidget {
+  const FeedScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CupertinoNavigationBar(
+        leading: IconButton(
+          onPressed: null,
+          icon: Icon(
+            CupertinoIcons.photo_camera_solid,
+            color: Colors.black87,
+          ),
+        ),
+        middle: Text('instagrams',
+          style: TextStyle(
+            fontFamily: 'VeganStyle',
+            color:Colors.black87,
+          ),
+        ),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            IconButton(
+                onPressed: null,
+                icon: ImageIcon(
+                  AssetImage('assets/images/actionbar_camera.png'),
+                  color:Colors.black87,
+                ),
+            ),
+            IconButton(
+              onPressed: null,
+              icon: ImageIcon(
+                AssetImage('assets/images/actionbar_camera.png'),
+                color:Colors.black87,
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: ListView.builder(
+        itemBuilder: feedListBuilder,
+        itemCount: 30,
+      ),
+    );
+  }
+
+  Widget feedListBuilder(BuildContext context, int index){
+    return Post(index);
+  }
+}
