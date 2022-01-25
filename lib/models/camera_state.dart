657ac3697922c7344a001624c646d104e236e1f6
tjdwhd70599@gmail.com <tjdwhd70599@gmail.com>
2022-01-26 06:32:10 +0900

    프로바이더 카메라

---
+++ lib/models/camera_state.dart
@@ -0,0 +1,48 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/foundation.dart';
+
+class CameraState extends ChangeNotifier{
+  late CameraController _controller;
+  late CameraDescription _cameraDescription;
+  bool _readyTakePhoto = false;
+
+  void getReadyToTakePhoto() async {
+    List<CameraDescription> cameras = await availableCameras();
+
+    if(cameras != null && cameras.isNotEmpty){
+      setCameraDescription(cameras[0]);
+    }
+    bool init = false;
+    while(!init){
+      init = await initialize();
+    }
+    _readyTakePhoto = true;
+    notifyListeners();
+  }
+
+  void setCameraDescription(CameraDescription cameraDescription){
+    _cameraDescription = cameraDescription;
+    _controller = CameraController(_cameraDescription, ResolutionPreset.medium);
+  }
+
+  Future<bool> initialize() async{
+    try{
+      await _controller.initialize();
+      return true;
+    }catch(e){
+      return false;
+    }
+  }
+
+  CameraController get controller => _controller;
+  CameraDescription get description => _cameraDescription;
+  bool get isReadyToTakePhoto => _readyTakePhoto;
+}
+
+
+// 1. available camera 가져오기 (Future)
+// 2. 카메라 리스트에서 첫번째 카메라 사용
+// 3. CameraController 인스턴스 생성
+// 4. CameraController.initialize()
+// 5. show preview
+// 6. set ready to take photo
--- lib/screens/camera_screen.dart
+++ lib/screens/camera_screen.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:instagrams/models/camera_state.dart';
 import 'package:instagrams/widget/take_photo.dart';
+import 'package:provider/provider.dart';
 
 class CameraScreen extends StatefulWidget {
-  const CameraScreen({Key? key}) : super(key: key);
+  CameraScreen({Key? key}) : super(key: key);
+
+  CameraState _cameraState = CameraState();
 
   @override
-  _CameraScreenState createState() => _CameraScreenState();
+  _CameraScreenState createState() {
+    _cameraState.getReadyToTakePhoto();
+    return _CameraScreenState();
+  }
 }
 
 class _CameraScreenState extends State<CameraScreen> {
@@ -23,38 +30,43 @@ class _CameraScreenState extends State<CameraScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(_title),
-      ),
-      body: PageView(
-        controller: _pageController,
-        children: <Widget>[
-          Container(
-            color: Colors.cyanAccent,
-          ),
-          TakePhoto(),
-          Container(
-            color: Colors.greenAccent,
-          ),
-        ],
-        // 탭버튼과 좌우스크롤 연동
-        onPageChanged: _connectTabNScroll,
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        iconSize: 0,
-        selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
-        selectedItemColor: Colors.black87,
-        unselectedItemColor: Colors.black54,
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.border_bottom_sharp), label: "GALLERY"),
-          BottomNavigationBarItem(icon: Icon(Icons.border_bottom_sharp), label: "PHOTO"),
-          BottomNavigationBarItem(icon: Icon(Icons.border_bottom_sharp), label: "VIDEO"),
-        ],
-        // 현재 위치를 변수로 할당
-        currentIndex: _currentIndex,
-        // 눌린 위치
-        onTap: _onItemTabbed,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<CameraState>.value(value: widget._cameraState),
+      ],
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(_title),
+        ),
+        body: PageView(
+          controller: _pageController,
+          children: <Widget>[
+            Container(
+              color: Colors.cyanAccent,
+            ),
+            TakePhoto(),
+            Container(
+              color: Colors.greenAccent,
+            ),
+          ],
+          // 탭버튼과 좌우스크롤 연동
+          onPageChanged: _connectTabNScroll,
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          iconSize: 0,
+          selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
+          selectedItemColor: Colors.black87,
+          unselectedItemColor: Colors.black54,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.border_bottom_sharp), label: "GALLERY"),
+            BottomNavigationBarItem(icon: Icon(Icons.border_bottom_sharp), label: "PHOTO"),
+            BottomNavigationBarItem(icon: Icon(Icons.border_bottom_sharp), label: "VIDEO"),
+          ],
+          // 현재 위치를 변수로 할당
+          currentIndex: _currentIndex,
+          // 눌린 위치
+          onTap: _onItemTabbed,
+        ),
       ),
     );
   }
